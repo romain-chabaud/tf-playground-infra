d018ac19a232b442970065caada3be7c74f6befb
Romain Chabaud <chabaud.r@sfeir.com>
2023-10-14 18:06:44 +0200

    feat: save secrets line by line instead of json

--- petclinic.tf
+++ petclinic.tf
@@ -15,8 +15,12 @@ resource "google_sql_user" "petclinic_user" {
 }
 
 # secret manager
-resource "google_secret_manager_secret" "petclinic_secret" {
-  secret_id = "petclinic_db_env_config"
+resource "google_service_account" "petclinic_secret_manager_service_account" {
+  account_id = "petclinic-secret-sa"
+}
+
+resource "google_secret_manager_secret" "petclinic_db_url_secret" {
+  secret_id = "PETCLINIC_DB_URL"
   replication {
     user_managed {
       replicas {
@@ -28,12 +32,64 @@ resource "google_secret_manager_secret" "petclinic_secret" {
   depends_on = [google_project_service.secret_manager_enabler]
 }
 
-resource "google_secret_manager_secret_version" "petclinic_secret_value" {
-  secret = google_secret_manager_secret.petclinic_secret.name
-  secret_data = jsonencode({
-    SPRING_PROFILES_ACTIVE = "postgres"
-    POSTGRES_URL           = "jdbc:postgresql://${google_sql_database_instance.shared_database_instance.public_ip_address}:5432/${google_sql_database.petclinic_database.name}"
-    POSTGRES_USER          = google_sql_user.petclinic_user.name
-    POSTGRES_PASS          = google_sql_user.petclinic_user.password
-  })
+resource "google_secret_manager_secret_version" "petclinic_db_url_secret_value" {
+  secret      = google_secret_manager_secret.petclinic_db_url_secret.name
+  secret_data = "jdbc:postgresql://${google_sql_database_instance.shared_database_instance.public_ip_address}:5432/${google_sql_database.petclinic_database.name}"
+}
+
+resource "google_secret_manager_secret_iam_member" "petclinic_db_url_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.petclinic_db_url_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.petclinic_secret_manager_service_account.email}"
+}
+
+resource "google_secret_manager_secret" "petclinic_db_user_secret" {
+  secret_id = "PETCLINIC_DB_USER"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [google_project_service.secret_manager_enabler]
+}
+
+resource "google_secret_manager_secret_version" "petclinic_db_user_secret_value" {
+  secret      = google_secret_manager_secret.petclinic_db_user_secret.name
+  secret_data = google_sql_user.petclinic_user.name
+}
+
+resource "google_secret_manager_secret_iam_member" "petclinic_db_user_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.petclinic_db_user_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.petclinic_secret_manager_service_account.email}"
+}
+
+resource "google_secret_manager_secret" "petclinic_db_password_secret" {
+  secret_id = "PETCLINIC_DB_PASSWORD"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [google_project_service.secret_manager_enabler]
+}
+
+resource "google_secret_manager_secret_version" "petclinic_db_password_secret_value" {
+  secret      = google_secret_manager_secret.petclinic_db_password_secret.name
+  secret_data = google_sql_user.petclinic_user.password
+}
+
+resource "google_secret_manager_secret_iam_member" "petclinic_db_password_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.petclinic_db_password_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.petclinic_secret_manager_service_account.email}"
 }
--- voting.tf
+++ voting.tf
@@ -15,8 +15,12 @@ resource "google_sql_user" "voting_user" {
 }
 
 # secret manager
-resource "google_secret_manager_secret" "voting_secret" {
-  secret_id = "voting_db_env_config"
+resource "google_service_account" "voting_secret_manager_service_account" {
+  account_id = "voting-secret-sa"
+}
+
+resource "google_secret_manager_secret" "voting_db_ip_secret" {
+  secret_id = "VOTING_DB_IP"
   replication {
     user_managed {
       replicas {
@@ -28,13 +32,89 @@ resource "google_secret_manager_secret" "voting_secret" {
   depends_on = [google_project_service.secret_manager_enabler]
 }
 
-resource "google_secret_manager_secret_version" "voting_secret_value" {
-  secret = google_secret_manager_secret.voting_secret.name
-  secret_data = jsonencode({
-    INSTANCE_HOST = google_sql_database_instance.shared_database_instance.public_ip_address
-    DB_PORT       = 5432
-    DB_NAME       = google_sql_database.voting_database.name
-    DB_USER       = google_sql_user.voting_user.name
-    DB_PASS       = google_sql_user.voting_user.password
-  })
+resource "google_secret_manager_secret_version" "voting_db_ip_secret_value" {
+  secret      = google_secret_manager_secret.voting_db_ip_secret.name
+  secret_data = google_sql_database_instance.shared_database_instance.public_ip_address
+}
+
+resource "google_secret_manager_secret_iam_member" "voting_db_ip_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.voting_db_ip_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.voting_secret_manager_service_account.email}"
+}
+
+resource "google_secret_manager_secret" "voting_db_name_secret" {
+  secret_id = "VOTING_DB_NAME"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [google_project_service.secret_manager_enabler]
+}
+
+resource "google_secret_manager_secret_version" "voting_db_name_secret_value" {
+  secret      = google_secret_manager_secret.voting_db_name_secret.name
+  secret_data = google_sql_database.voting_database.name
+}
+
+resource "google_secret_manager_secret_iam_member" "voting_db_name_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.voting_db_name_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.voting_secret_manager_service_account.email}"
+}
+
+resource "google_secret_manager_secret" "voting_db_user_secret" {
+  secret_id = "VOTING_DB_USER"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [google_project_service.secret_manager_enabler]
+}
+
+resource "google_secret_manager_secret_version" "voting_db_user_secret_value" {
+  secret      = google_secret_manager_secret.voting_db_user_secret.name
+  secret_data = google_sql_user.voting_user.name
+}
+
+resource "google_secret_manager_secret_iam_member" "voting_db_user_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.voting_db_user_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.voting_secret_manager_service_account.email}"
+}
+
+resource "google_secret_manager_secret" "voting_db_password_secret" {
+  secret_id = "VOTING_DB_PASSWORD"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+
+  depends_on = [google_project_service.secret_manager_enabler]
+}
+
+resource "google_secret_manager_secret_version" "voting_db_password_secret_value" {
+  secret      = google_secret_manager_secret.voting_db_password_secret.name
+  secret_data = google_sql_user.voting_user.password
+}
+
+resource "google_secret_manager_secret_iam_member" "voting_db_password_secret_member" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.voting_db_password_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.voting_secret_manager_service_account.email}"
 }
